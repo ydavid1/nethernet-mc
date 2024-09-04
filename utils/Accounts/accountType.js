4f469f62479095402d9b69d489a4a1e325edfade
ydavid1 <ydavid@nethernet.org>
2024-09-03 22:18:34 -0500

    Added GetAccountUser function

--- utils/Accounts/accountType.js
+++ utils/Accounts/accountType.js
@@ -1,7 +1,23 @@
+const DiscordUser = require("../../database/schemas/DiscordUser")
+const LocalUser = require("../../database/schemas/LocalUser")
+
 function detectAccountType(req) {
     return req.session.passport.user.type
 }
 
+async function getAccountUser(req) {
+    const accType = detectAccountType(req)
+    let user = null
+    if (accType == "discord") {
+        user = await DiscordUser.findById(req.session.passport.user.id);
+    } else if (accType == "local") {
+        user = await LocalUser.findById(req.session.passport.user.id);
+    }
+
+    return user
+}
+
 module.exports = {
-    detectAccountType
+    detectAccountType,
+    getAccountUser
 }
